4eae50b3a0abc5e980b47a20828ccafff219c172
AaronMD98 <AaronLev.210@outlook.com>
2024-04-10 09:53:25 +1000

    test commit redirect to the station url

--- app/admin/route.ts
+++ app/admin/route.ts
@@ -1,8 +1,29 @@
 import { redirect } from 'next/navigation';
 
 const canonicalDomain: string = process.env.BIGCOMMERCE_GRAPHQL_API_DOMAIN ?? 'mybigcommerce.com';
-const BIGCOMMERCE_STORE_HASH = process.env.BIGCOMMERCE_STORE_HASH;
-const ENABLE_ADMIN_ROUTE = process.env.ENABLE_ADMIN_ROUTE;
+const BIGCOMMERCE_STORE_HASH = process.env.BIGCOMMERCE_STORE_HASH ?? '';
+const ENABLE_ADMIN_ROUTE = process.env.ENABLE_ADMIN_ROUTE === 'true'; // Convert to boolean directly
+const SITE_ID = process.env.CHANNEL_ID ?? '';
+const ACCESS_TOKEN = process.env.BIGCOMMERCE_ACCESS_TOKEN;
+
+const fetch = require('node-fetch');
+
+let url = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/sites/${SITE_ID}`;
+
+let options = {
+  method: 'PUT',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'X-Auth-Token': `${ACCESS_TOKEN}`,
+  },
+  body: `{"url":"https://big-commerce-atld8noeg-aaronmd98s-projects.vercel.app"}`,
+};
+
+fetch(url, options)
+  .then((res: any) => res.json())
+  .then((json: any) => console.log(json))
+  .catch((err: string) => console.error('error:' + err));
 
 export const GET = () => {
   // This route should not work unless explicitly enabled
