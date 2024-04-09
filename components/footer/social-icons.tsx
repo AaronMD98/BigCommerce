6403d191a94eecf93b054838231ac54f3249feb8
AaronMD98 <AaronLev.210@outlook.com>
2024-04-10 09:32:49 +1000

    inital commit

--- components/footer/social-icons.tsx
+++ components/footer/social-icons.tsx
@@ -23,23 +23,53 @@ const socialIconNames = [
 const SocialIcon = ({ name }: { name: string }) => {
   switch (name) {
     case 'Facebook':
-      return <SiFacebook title="Facebook" />;
+      return (
+        <SiFacebook
+          title="Facebook"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+      );
 
     case 'Twitter':
     case 'X':
-      return <SiX title="X" />;
+      return <SiX title="X" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />;
 
     case 'Pinterest':
-      return <SiPinterest title="Pinterest" />;
+      return (
+        <SiPinterest
+          title="Pinterest"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+      );
 
     case 'Instagram':
-      return <SiInstagram title="Instagram" />;
+      return (
+        <SiInstagram
+          title="Instagram"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+      );
 
     case 'LinkedIn':
-      return <SiLinkedin title="LinkedIn" />;
+      return (
+        <SiLinkedin
+          title="LinkedIn"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+      );
 
     case 'YouTube':
-      return <SiYoutube title="YouTube" />;
+      return (
+        <SiYoutube
+          title="YouTube"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+      );
 
     default:
       return null;
