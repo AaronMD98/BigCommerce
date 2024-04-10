2e960ad553a38d8eca81dd75b2f1885656d6ccdb
AaronMD98 <AaronLev.210@outlook.com>
2024-04-10 12:13:53 +1000

    commit

--- app/api/index.ts
+++ app/api/index.ts
@@ -2,33 +2,57 @@ const BIGCOMMERCE_STORE_HASH = process.env.BIGCOMMERCE_STORE_HASH ?? '';
 // Convert to boolean directly
 const ENABLE_ADMIN_ROUTE = process.env.ENABLE_ADMIN_ROUTE === 'true';
 const SITE_ID = process.env.BIGCOMMERCE_CHANNEL_ID;
-
-const ACCESS_TOKEN = process.env.BIGCOMMERCE_ACCESS_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 // Assuming you want to fetch information for a specific site, you should include SITE_ID in your URL
-const url: string = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/sites/${SITE_ID}`;
+const url: string = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/sites/1001`;
+
+///// GET URL
+const urlGET: string = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/channels/${SITE_ID}/site`;
+
+///// POST URL
+const urlPOST: string = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/sites/${SITE_ID}/routes`;
 
 let headers: Record<string, any> = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
-  'X-Auth-Token': ACCESS_TOKEN, // Directly assign ACCESS_TOKEN with a default fallback
+  'X-Auth-Token': ACCESS_TOKEN,
 };
 
-// Define the options for fetch call
 const options = {
   method: 'GET',
-  headers, // Use the previously defined headers object
+  headers,
+};
+
+const optionsPOST = {
+  method: 'POST',
+  headers,
+};
+
+const optionsPUT = {
+  method: 'PUT',
+  headers,
+  body: `{"url":"https://big-commerce-git-main-aaronmd98s-projects.vercel.app"
+  }`,
 };
 
 export async function fetchSites() {
   try {
-    const response = await fetch(url, options);
+    // GET THE SITE CHANNEL
+    // const res = await fetch(urlGET, options);
+    // UPDATE THE SITE URL
+    const response = await fetch(url, optionsPUT);
+
     if (!response.ok) {
       // This will catch HTTP errors such as 404, 500 etc.
-      throw new Error(`Error: ${response.statusText} : ${url}`);
+      throw new Error(`Error: ${response.statusText} : ${urlGET}`);
     }
     const json = await response.json();
-    console.log('OUTPUT', json);
+    // const jsonGET = await res.json();
+
+    ///// PRINT THE OUTPUTS
+    // console.log('OUTPUT (GET):', jsonGET);
+    console.log('OUTPUT (UPDATE):', json);
   } catch (err) {
     // This will catch network errors and errors thrown from the above throw statement
     console.error('Error:', err);
